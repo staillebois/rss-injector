0efb3e08118148a0b3093c5add3c5ebb4def7573
staillebois <sylvain.taillebois@gmail.com>
2025-02-14 07:28:01 +0100

    Add idempotentConsumer to avoid duplicate entries

--- src/main/java/com/st/RssKafkaRoute.java
+++ src/main/java/com/st/RssKafkaRoute.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.logging.Logger;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -31,6 +32,8 @@ public class RssKafkaRoute extends RouteBuilder {
                         .to("xj:identity?transformDirection=XML2JSON")
                         .transform().jq(xmlJqFilter)
                         .process(avroProcessor)
+                        // .log("Processing ${body.get('title')}")
+                        .idempotentConsumer((simple("${body.get('title')}")), MemoryIdempotentRepository.memoryIdempotentRepository(200))
                         .to("kafka:{{kafka.topic}}?brokers={{kafka.broker}}&schemaRegistryURL={{schema.registry.url}}&valueSerializer={{kafka.value.serializer}}"));
 
     }
